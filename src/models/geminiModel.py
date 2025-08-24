68668ea7762bcccb4d42ca1205cb1590b5c10a11
nathsujal <sujalnath108@gmail.com>
2025-08-25 01:25:33 +0530

    feat: add gemini model to work with Google Gemini models

---
+++ src/models/geminiModel.py
@@ -0,0 +1,38 @@
+"""
+Groq model implementation using the official LangChain integration.
+"""
+import os
+
+from google import genai
+from google.genai import types
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import logging
+logger = logging.getLogger(__name__)
+
+class GeminiModel():
+    """
+    Groq API model implementation using langchain-groq.
+    """
+
+    def __init__(self):
+        self._load_model()
+
+    def _load_model(self) -> None:
+        """Initializes the ChatGroq client from LangChain."""
+        self.client = genai.Client(
+            api_key=os.environ.get("GEMINI_API_KEY"),
+        )
+        self.model = "gemma-3-4b-it"
+
+    def generate(self, prompt: str, **kwargs) -> str:
+        """Generates text for a single prompt from the Groq API."""
+        response = self.client.models.generate_content(
+            model=self.model,
+            contents=prompt,
+        )
+        logger.info(f"Generated response: {response.text[:50]}...")
+        return response.text
+
